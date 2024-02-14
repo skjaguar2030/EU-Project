71031b29c6c279aa22b119b89b4729e94d71a793
skjaguar2030 <seifira21@protonmail.com>
2024-02-14 15:39:34 +0200

    My first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+//   template: "<h2>I am a testing Template</h2>",
+
+  data() {
+    return {
+        showBooks: true,
+        title: "The Final Empire",
+        author: "Brandon Senderson",
+        age: 45
+    }
+  },
+
+  methods: {
+    changeTitle () {
+        this.title = "Wakanda Forever"
+    },
+    changeAuthor () {
+        this.author = "Wakanda Forever"
+    },
+
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks
+    },
+
+    // handleEvent(e) {
+    //   console.log(e);
+    // }
+  }
+});
+
+app.mount("#app");
